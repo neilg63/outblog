fcc997911d21286276efad5d6187da737ac54bd8
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-06-16 16:54:08 +0100

    worked on compoents

--- src/components/layout/CustomHead.tsx
+++ src/components/layout/CustomHead.tsx
@@ -1,6 +1,6 @@
 import { Accessor, For } from "solid-js";
 import { MetaTagSet } from "../../api/models";
-import { Meta, Title } from "solid-start";
+import { Link, Meta, Title } from "solid-start";
 
 export default function CustomHead({ meta }: { meta: Accessor<MetaTagSet | undefined> }) {
   return <>
@@ -13,6 +13,12 @@ export default function CustomHead({ meta }: { meta: Accessor<MetaTagSet | undef
       <Meta property="twitter:description" content={meta()?.description} />
       <Meta property="og:image" content={meta()?.image} />
       <Meta property="twitter:card" content="summary_large_image" />
-      <Meta name="twitter:image" content={meta()?.image} />
+    <Meta name="twitter:image" content={meta()?.image} />
+    	<Link rel="apple-touch-icon" sizes="180x180" href="/uploads/fbrfg/apple-touch-icon.png" />
+      <Link rel="icon" type="image/png" sizes="32x32" href="/uploads/fbrfg/favicon-32x32.png" />
+      <Link rel="icon" type="image/png" sizes="16x16" href="/uploads/fbrfg/favicon-16x16.png" />
+      <Link rel="manifest" href="/uploads/fbrfg/site.webmanifest" />
+      <Link rel="mask-icon" href="/uploads/fbrfg/safari-pinned-tab.svg" color="#5bbad5" />
+      <Link rel="shortcut icon" href="/uploads/fbrfg/favicon.ico" />
   </>;
 }
